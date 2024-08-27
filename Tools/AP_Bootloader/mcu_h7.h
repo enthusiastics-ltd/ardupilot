cbb8bcf13e00ae7371f8a6bd22bcc0a3e09a08cf
Markiian Benovskyi <2558086+MarkusBansky@users.noreply.github.com>
2024-08-27 10:00:15 +0100

    WIP on adfding H7S3 support for ardupilot

--- Tools/AP_Bootloader/mcu_h7.h
+++ Tools/AP_Bootloader/mcu_h7.h
@@ -7,11 +7,13 @@
 #define STM32_UNKNOWN 0
 #define STM32_H743    0x450
 #define STM32_H730    0x483
+#define STM32_H7S3    0x485
 
 mcu_des_t mcu_descriptions[] = {
     { STM32_UNKNOWN,     "STM32H7???"},
     { STM32_H730,        "STM32H73x/72x"},
     { STM32_H743,        "STM32H743/753/750"},
+    { STM32_H7S3,        "STM32H7S3"},
 };
 
 const mcu_rev_t silicon_revs[] = {
